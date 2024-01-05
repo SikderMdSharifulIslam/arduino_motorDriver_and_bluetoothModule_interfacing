ac24fec23ed9022bdf46c0dffc60e5a16d7ad89c
sikdermdsharifulislam <sikdermdsharifulislam@gmail.com>
2024-01-06 04:08:05 +0600

    arduino file

---
+++ arduino_car.ino
@@ -0,0 +1,59 @@
+int motorIN1 = 12;
+int motorIN2 = 11;
+int motorIN3 = 10;
+int motorIN4 = 9;
+
+char State;
+
+
+void setup() {
+  pinMode (motorIN1, OUTPUT);
+  pinMode (motorIN2, OUTPUT);
+  pinMode (motorIN3, OUTPUT);
+  pinMode (motorIN4, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 0)
+  {
+    State = Serial.read();
+  }
+  if(State == 'f') //forward
+  {
+    digitalWrite(motorIN1, LOW);
+    digitalWrite(motorIN2, HIGH);
+    digitalWrite(motorIN3, LOW);
+    digitalWrite(motorIN4, HIGH);
+  }
+  else if(State == 'b') //backward
+  {
+    digitalWrite(motorIN1, HIGH);
+    digitalWrite(motorIN2, LOW);
+    digitalWrite(motorIN3, HIGH);
+    digitalWrite(motorIN4, LOW);
+  }
+  else if(State == 'l') //turn left
+  {
+    digitalWrite(motorIN1, HIGH);
+    digitalWrite(motorIN2, LOW);
+    digitalWrite(motorIN3, LOW);
+    digitalWrite(motorIN4, HIGH);
+  }
+  else if(State == 'r') //turn right
+  {
+    digitalWrite(motorIN1, LOW);
+    digitalWrite(motorIN2, HIGH);
+    digitalWrite(motorIN3, HIGH);
+    digitalWrite(motorIN4, LOW);
+  }
+  else
+  {
+    digitalWrite(motorIN1, LOW);
+    digitalWrite(motorIN2, LOW);
+    digitalWrite(motorIN3, LOW);
+    digitalWrite(motorIN4, LOW);
+  }
+
+
+}
